a37f4943cfaa46c12afdf3cc61b8747fce45b56e
Jamal <jayjamal86@gmail.com>
2022-10-24 18:24:53 +0300

    airbnb

---
+++ models/base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module contains the BaseModel class. """
+import uuid
+
+
+class BaseModel:
+    """ This class will define all common
+    attributes/methods for other classes. """
+
+    def __init__():
+        """__init__ _summary_."""
+        pass
