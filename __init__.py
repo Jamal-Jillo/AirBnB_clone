d794a823c47968fecc397178ddea67aeb4bfc706
Jamal <jayjamal86@gmail.com>
2022-10-24 21:38:30 +0300

    fixes

--- __init__.py
+++ __init__.py
@@ -1,1 +1,3 @@
-"empty"
+"""
+_summary_
+"""
--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """This module contains the BaseModel class."""
 from datetime import datetime
-from models import storage
 import uuid
 
 
