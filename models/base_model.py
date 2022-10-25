0854ba736aa8c6abbd729f1c8d7a9e263bdee0b3
Jamal <jayjamal86@gmail.com>
2022-10-25 08:58:59 +0300

    base_model from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -12,9 +12,16 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """__init__ constructor method for BaseModel class."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs != {}:  # if kwargs is not empty
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """__str__ method to print a string representation of the class."""
