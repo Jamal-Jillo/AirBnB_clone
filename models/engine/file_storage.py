c5544f07ed10f9192ecf9fd9b613449e79407fed
Jamal <jayjamal86@gmail.com>
2022-10-26 15:13:50 +0300

    storage features

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,22 +10,22 @@ class FileStorage:
 
     def all(self):
         """ Returns the dictionary __objects."""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """ Sets in __objects the obj with key <obj class name>.id. """
-        key = obj.__class__.__name__ + "." + obj.id
-        FileStorage.__objects[key] = obj
+        self.__objects[type(obj).__name__ + "." + obj.id] = obj
 
     def save(self):
         """ Serializes __objects to the JSON file (path: __file_path). """
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(FileStorage.__objects, f)
+        temp = {}
+        with open(self.__file_path, 'w') as jsonfile:
+            json.dump(temp, jsonfile)
 
     def reload(self):
         """ Deserializes the JSON file to __objects. """
         try:
-            with open(FileStorage.__file_path, 'r') as f:
-                FileStorage.__objects = json.load(f)
+            with open(self.__file_path, 'r') as f:
+                self.__objects = json.load(f)
         except FileNotFoundError:
             pass
