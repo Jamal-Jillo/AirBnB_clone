65d82d84de5c0f44b5694ce772a87c7fc7b035a8
Jamal <jayjamal86@gmail.com>
2022-10-27 16:46:01 +0300

    command line checks

--- console.py
+++ console.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """Console module for the AirBnB project."""
 import cmd
+import models
+import shlex  # For splitting arguments passed
+from models.base_model import BaseModel
+
+classes = {"BaseModel": BaseModel}
 
 
 class HBNBCommand(cmd.Cmd):
@@ -33,6 +38,43 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Empty line + ENTER shouldn’t execute anything."""
         pass
+
+    def do_create(self, args):
+        """Create a new instance of a BaseModel."""
+        if args == "":
+            print("** class name missing **")
+        elif args != "BaseModel":
+            print("** class doesn't exist **")
+
+# ? Update this later
+    def help_create(self):
+        """Help for create command."""
+        print("Create a new instance of a BaseModel")
+
+    def do_show(self, arg):
+        """Print the string representation of an instance."""
+        args = shlex.split(arg)  # Split arguments passed
+        if len(args) == 0:  # If no arguments passed
+            print("** class name missing **")
+            return False
+        if args[0] in classes:  # If class name is valid (in classes)
+            if len(args) > 1:  # Check If id is passed
+                # Create key to search for in storage
+                key = args[0] + "." + args[1]
+                # Check If key exists in storage
+                if key in models.storage.all():
+                    print(models.storage.all()[key])
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
+
+# ? Update this later
+    def help_show(self):
+        """Help for show command."""
+        print("Print the string representation of an instance")
 
 
 if __name__ == '__main__':
