529bb8b02e6f7ce27145d7533c41194274f31285
Jamal <jayjamal86@gmail.com>
2022-10-28 23:18:37 +0300

    Added all classes.

--- console.py
+++ console.py
@@ -5,8 +5,15 @@ import models
 import shlex  # For splitting arguments passed
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
-classes = {"BaseModel": BaseModel, "User": User}  # Dictionary of classes
+classes = {"BaseModel": BaseModel, "User": User, "State": State, "City": City,
+           "Place": Place, "Amenity": Amenity, "Review": Review}
+# -- needs to be updated with all classes
 
 
 class HBNBCommand(cmd.Cmd):
